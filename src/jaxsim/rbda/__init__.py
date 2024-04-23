c6fee3c65403c3eef16e1eabd54ce5fef2254360
Filippo Luca Ferretti <filippoluca.ferretti@outlook.com>
2024-04-23 11:01:02 +0200

    Move `soft_contacts` and `rigid_contacts` to `api`

--- src/jaxsim/rbda/__init__.py
+++ src/jaxsim/rbda/__init__.py
@@ -4,4 +4,3 @@ from .crba import crba
 from .forward_kinematics import forward_kinematics, forward_kinematics_model
 from .jacobian import jacobian, jacobian_full_doubly_left
 from .rnea import rnea
-from .soft_contacts import SoftContacts, SoftContactsParams
